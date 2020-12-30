00e4aaef3cdd7c1f94bd44feb66e7b5f3bcc5f23
leokattah <leokattah@gmail.com>
2020-12-29 23:29:39 -0300

    day 4 adicionado

--- 30 days of code/Day 4 Class vs. Instance/age.js
+++ 30 days of code/Day 4 Class vs. Instance/age.js
@@ -19,14 +19,45 @@ function readLine() {
 }
 
 function Person(initialAge){
-    // Add some more code to run some checks on initialAge
+        // Add some more code to run some checks on initialAge
+        // o nome da classe é "Person" e a instância é "Age"
+        // o parâmetro do construtor é "initialAge"
+        if (initialAge < 0 ){
+            age = 0;
+            console.log("Age is not valid");
+        } else {
+            age = 0;
+        }
   this.amIOld=function(){
    // Do some computations in here and print out the correct statement to the console
-
+    if (age < 13){
+    console.log("You are young.");
+    }
+    else if (age >=13 && age < 18) {
+    console.log("You are a teenager.")
+    }
+    else {
+    console.log("You are old.")
+    }
   };
    this.yearPasses=function(){
           // Increment the age of the person in here
+        
+
    };
 }
 
 function main() {
+    var T=parseInt(readLine());
+for(i=0;i<T;i++){
+    var age=parseInt(readLine());
+    var p=new Person(age);
+    p.amIOld();
+    for(j=0;j<3;j++){
+        p.yearPasses();
+        
+    }
+    p.amIOld();
+    console.log("");   
+    }
+}
