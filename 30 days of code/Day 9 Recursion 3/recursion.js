b2bd8f310deb1207e93dae792d1350ee1ebd19c3
leokattah <leokattah@gmail.com>
2021-01-03 20:43:27 -0300

    problema dia 9 resolvido

--- 30 days of code/Day 9 Recursion 3/recursion.js
+++ 30 days of code/Day 9 Recursion 3/recursion.js
@@ -27,7 +27,14 @@ function readLine() {
 // Complete the factorial function below.
 function factorial(n) {
 
-
+    //base case.
+    if (n <= 1){
+        return 1;
+    }
+    //recursive case.
+    else {
+        return n * factorial(n-1)
+    }
 }
 
 function main() {
