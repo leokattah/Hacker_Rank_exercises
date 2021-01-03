7603dc3291056b6ba190f8ef02701288206aab8d
leokattah <leokattah@gmail.com>
2021-01-03 18:06:12 -0300

    problema 9 adicionado

--- 30 days of code/Day 8 Dictionaries and Maps/dictionariesAndMaps.js
+++ 30 days of code/Day 8 Dictionaries and Maps/dictionariesAndMaps.js
@@ -6,22 +6,21 @@ function processData(input) {
   //coloca essas substrings em um array e retorna o array. A divisão é feita procurando um padrão,
   // onde o padrão é fornecido como o primeiro parâmetro na chamada do método.
   // input é parâmetro da função processData(input).
-  
+
 input = input.split('\n');
   let phoneBook =  [];
-  
-    for(i=1;i<=parseInt(input[0]);i++) {
+      for(i=1;i<=parseInt(input[0]);i++) {
       let contactArray =input[i].split(' ');
       phoneBook[contactArray[0]] = contactArray[1];
   }
 
-  for(i=(parseInt(input[0])+1);i<input.length;i++){
-      if(phoneBook[input[i]]) {
+      for(i=(parseInt(input[0])+1);i<input.length;i++){
+        if(phoneBook[input[i]]) {
           console.log(input[i] + '=' + phoneBook[input[i]]);
-      }else{
-          console.log('Not found');
-      }
-  }
+        }else{
+        console.log('Not found');
+        }
+       }
 }
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
