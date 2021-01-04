735dff11d0354af45a2f18230835ac74f6b6423d
leokattah <leokattah@gmail.com>
2021-01-04 15:34:18 -0300

    day 10 resolvido

---
+++ 30 days of code/Day 10 Binary Numbers/binaryNumbers.js
@@ -0,0 +1,31 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    
+       console.log(Math.max.apply(null, n.toString(2).split("0").map(x => x.length)));
+} 
+    
+
