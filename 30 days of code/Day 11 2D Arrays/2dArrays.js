627c68d286d0c61df6aa29caafba6b1c7988477c
leokattah <leokattah@gmail.com>
2021-01-05 16:09:07 -0300

    day 11 2D arrys solved

--- 30 days of code/Day 11 2D Arrays/2dArrays.js
+++ 30 days of code/Day 11 2D Arrays/2dArrays.js
@@ -22,12 +22,26 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-
-
 function main() {
-    let arr = Array(6);
-
-    for (let i = 0; i < 6; i++) {
-        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    var arr = [];
+    var arr_i = [];
+    for(arr_i = 0; arr_i < 6; arr_i++){
+       arr[arr_i] = readLine().split(' ');
+       arr[arr_i] = arr[arr_i].map(Number);
     }
+    var greatestSum = null;
+  
+  for (var i = 0; i < arr.length - 2; i++) {
+    for (var j = 0; j < arr[i].length - 2; j++) {
+      var top = arr[i][j] + arr[i][j + 1] + arr[i][j + 2],
+          mid = arr[i + 1][j + 1],
+          bot = arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2],
+          total = top + mid + bot;
+
+      if (greatestSum === null || total > greatestSum) {
+        greatestSum = total;
+      }
+    }
+  }
+  console.log(greatestSum);
 }
