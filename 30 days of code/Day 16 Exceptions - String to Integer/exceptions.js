a643d629b9a3b4994e6cb944c2e680489be19a94
leokattah <leokattah@gmail.com>
2021-01-10 16:22:24 -0300

    day 16 solved

--- 30 days of code/Day 16 Exceptions - String to Integer/exceptions.js
+++ 30 days of code/Day 16 Exceptions - String to Integer/exceptions.js
@@ -20,23 +20,10 @@ function readLine() {
 
 /////////////// ignore above this line ////////////////////
 
-/*
- * You can use the following pseudocode:
- *
- * `S` = read string from stdin.
- * try {
- *     `N` = numeric value of `S`.
- *     
- *     If `N` is `NaN` or `undefined`, then some exception must be thrown,
- *     you can write the following withtout using conditional statement:
- *     <condition to return true when `N` is `NaN` or `undefined`> && an_undefined_function_call()
- *
- *     print `N`
- * } catch (err) {
- *     print `Bad String`.
- * }
- */
-
 function main() {
     const S = readLine();
+    const num = Number(S);
+    isNaN(num) ? console.log('Bad String') : console.log(num);
 }
+    
+    
