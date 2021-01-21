41ade195f0ec9284e17641973430cf7ded222254
leokattah <leokattah@gmail.com>
2021-01-20 22:04:40 -0300

    day 26 solved

--- 30 days of code/Day 26 Nested Logic/nestedLogic.js
+++ 30 days of code/Day 26 Nested Logic/nestedLogic.js
@@ -1,9 +1,30 @@
 function processData(input) {
-  //Enter your code here
-  
-  
-} 
+  const lines = input.split('\n');
+  const actualReturned = lines[0].split(' ').map(Number);
+  const expectedReturn = lines[1].split(' ').map(Number);
 
+  if (actualReturned[2] > expectedReturn[2]) {
+      console.log(10000);
+  } 
+  else if (actualReturned[2] < expectedReturn[2]) {
+      console.log(0);
+  } 
+  else if (actualReturned[1] > expectedReturn[1]) {
+      const diff = actualReturned[1] - expectedReturn[1];
+      console.log(diff * 500);
+  } 
+  else if (actualReturned[1] < expectedReturn[1]) {
+      console.log(0);
+  } 
+  else if (actualReturned[0] > expectedReturn[0]) {
+      const diff = actualReturned[0] - expectedReturn[0];
+      console.log(diff * 15);
+  } 
+  else {
+      console.log(0);
+  }
+} 
+   
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
 _input = "";
